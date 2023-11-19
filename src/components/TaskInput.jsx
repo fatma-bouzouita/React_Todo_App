a452cef80dea9f34cdb54416564fe9ac60851522
fatma <bouzouitafatma5@gmail.com>
2023-11-19 23:24:43 +0100

    add taskinput

---
+++ src/components/TaskInput.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const TaskInput = () =>  {
+    return (
+<form className="inputField">
+    < input type ="text" placeholder="Add a Task"/>
+    <button> 
+        +
+    </button>   
+</form>
+    );
+};
+export default TaskInput;
