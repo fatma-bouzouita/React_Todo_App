8f1a257442ad3d941075c57080aee3454f66b710
fatma <bouzouitafatma5@gmail.com>
2023-11-23 00:27:36 +0100

    check todoitem

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,18 @@ function App() {
     const newTask = { taskName, checked: false };
     setTodoList([...toDoList, newTask])
   };
-  function deleteTask(deleteTaskName)
-  {setTodoList(toDoList.filter(task  => task.taskName!== deleteTaskName))}
+  function deleteTask(deleteTaskName) { setTodoList(toDoList.filter(task => task.taskName !== deleteTaskName)) }
   console.log(toDoList);
+
+  function toggleCheck(taskName) {
+    setTodoList((prevTodoLIist) =>
+      prevTodoLIist.map((task) =>
+        task.taskName === taskName ? { ...task, checked: !task.checked } : task,
+      ),
+    );
+    const updatedTask = toDoList.find((task) => task.taskName === taskName);
+    console.log(updatedTask ? updatedTask.checked : null);
+  }
   return (
 
     <div className="container">
@@ -21,13 +30,14 @@ function App() {
         <span> To DO  </span>
         <ul className="list-items">
           {toDoList.map((task, key) => (
-            <TaskItem task={task} key={key}  deleteTask={deleteTask}/>
+            <TaskItem task={task} key={key} deleteTask={deleteTask}
+              toggleCheck={toggleCheck} />
           ))}
         </ul>
         <div>
           {toDoList.length === 0 ?
             (<div className="notify">
-              <p>  Congratulations! You've completed all tasks. </p>
+              <p>  Congratulations! You've completed all tasks ! </p>
               <FaThumbsUp className="icon" />
             </div>
             ) : null}
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import {MdDeleteSweep} from 'react-icons/md'
+import { MdDeleteSweep } from 'react-icons/md'
 
-const TaskItem = ({ task,deleteTask }) => {
+const TaskItem = ({ task, deleteTask,toggleCheck }) => {
   return (
-<li className="items">
-  <div className="checkbox-container">
-    <input type="checkbox" />
-  </div>
-  <div className="items-text">
-    <div>{task.taskName}</div>
-    <MdDeleteSweep className="delete-icon" onClick={() =>deleteTask(task.taskName)} />
-  </div>
-</li>
+    <li className="items">
+      <div className="checkbox-container">
+        <input type="checkbox" checked={task.checked} onChange={() =>toggleCheck(task.taskName)}/>
+      </div>
+      <div className="items-text">
+        <p className={task.checked ? 'isChecked' : ''}>{task.taskName} </p>
+        <MdDeleteSweep className="delete-icon" onClick={() => deleteTask(task.taskName)} />
+      </div>
+    </li>
 
   )
 }
